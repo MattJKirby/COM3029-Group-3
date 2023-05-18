8ccb4ce6cc48d72247c17ee1a0ae342bc1bb7030
Matt Kirby <MattJKirby@outlook.com>
2023-05-18 15:13:30 +0100

    Added status endpoint

--- web-service/app.py
+++ web-service/app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, make_response
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+@app.route("/status")
+def status():
+    response = make_response('OK', 200)
+    return response

82d91322b54eb40abbc30ad3900a22e64daf7e67
Matt Kirby <MattJKirby@outlook.com>
2023-05-18 16:29:28 +0100

    Better timestamp parsing

--- web-service/src/logger.py
+++ web-service/src/logger.py
@@ -1,16 +1,21 @@
 import time
+from datetime import datetime
 
 class Logger: 
   def __init__(self):
-    self.timestamp = time.time()
+    self.timestamp = self._parseTimestamp(time.time())
     self.logFile = f"{self.timestamp}.txt"
     self.logFilePath = f"logs/{self.logFile}"
     self._init()
 
+  def _parseTimestamp(self, timestamp):
+    dateTime = datetime.fromtimestamp(timestamp)
+    return dateTime.strftime("%Y-%m-%d %H:%M:%S")
+
   
   def _log(self, item):
-    with open(self.logFilePath, 'w') as file:
-      file.write(f"{self.timestamp} - {item}")
+    with open(self.logFilePath, 'a') as file:
+      file.write(f"{self.timestamp} - {item} \n")
 
   def _init(self):
     self._log("Service started")

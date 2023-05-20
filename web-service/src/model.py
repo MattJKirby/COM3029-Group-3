e6d85f5fe39359d5e015c8f29843eaa0896262ef
Matt Kirby <MattJKirby@outlook.com>
2023-05-20 21:37:25 +0100

    fixed prediction

--- web-service/src/model.py
+++ web-service/src/model.py
@@ -1,10 +1,12 @@
 from tensorflow import keras
-from transformers import AutoTokenizer
+from keras.models import load_model, Model
+from keras.layers import Input, Dense
+from transformers import AutoTokenizer, TFRobertaForSequenceClassification
 import numpy as np
 
 class Model:
     def __init__(self,modelPath):
-        self.model = keras.models.load_model(modelPath)
+        self.model = load_model(modelPath)
         self._tokenizer = AutoTokenizer.from_pretrained("roberta-base")
         self._classes = ['sadness', 'confusion', 'joy', 'anger', 'optimism', 'disapproval', 'love', 'curiosity', 'amusement', 'annoyance', 'gratitude', 'approval', 'admiration', 'neutral']
         
@@ -12,6 +14,6 @@ class Model:
         return self._tokenizer([input], padding = "max_length", truncation = True, max_length = 31)
 
     def predict(self, input):
-        tokenized = self._tokenize(input)
-        prediction = self.model.predict([np.array(tokenized["attention_mask"]), np.array(tokenized["input_ids"])])
-        return self._classes[np.argmax(prediction)]
+        tokenized = self._tokenizer([input], padding = "max_length", truncation = True, max_length = 31)
+        raw_prediction = self.model.predict([np.array(tokenized["input_ids"]), np.array(tokenized["attention_mask"])], verbose = 0)
+        return self._classes[np.argmax(raw_prediction)]

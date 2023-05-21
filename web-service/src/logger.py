0d5861ad4aedaaa6ab93bad35c6ab95718d637dc
Nordsword3m <doublethe3@gmail.com>
2023-05-21 21:01:05 +0100

    Create logs folder if it doesn't exist

--- web-service/src/logger.py
+++ web-service/src/logger.py
@@ -1,3 +1,4 @@
+import os
 import time
 from datetime import datetime
 
@@ -6,6 +7,10 @@ class Logger:
     self.timestamp = self._parseTimestamp(time.time())
     self.logFile = f"{self.timestamp}.txt"
     self.logFilePath = f"logs/{self.logFile}"
+
+    if not os.path.isdir("logs"):
+      os.mkdir("logs")
+
     self._init()
 
   def _parseTimestamp(self, timestamp):
